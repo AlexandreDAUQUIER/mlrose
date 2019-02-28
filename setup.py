d1b5dc968afc13720a537ca43cda2692eeaa3d7e
AlexandreDAUQUIER <47509691+AlexandreDAUQUIER@users.noreply.github.com>
2019-02-28 23:37:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def readme():
 
 
 setup(name='mlrose',
-      version='1.0.3',
+      version='1.0.4',
       description="MLROSe: Machine Learning, Randomized Optimization and"
       + " Search",
       long_description=readme(),
